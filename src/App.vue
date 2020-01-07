ae299279a3b7f0cafbf917cb5b4b55dfa35c3ea3
Martin <martinlcalvert@gmail.com>
2020-01-06 23:19:35 -0800

    Add 2019 versions

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,75 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <md-toolbar class="md-dense">
+      <h3 class="md-title" style="flex: 1">Super Bowl Squares</h3>
+      <md-button class="md-raised md-primary" @click="choose(10)">10 Pool</md-button>
+      <md-button class="md-raised md-primary" @click="choose(25)">25 Pool</md-button>
+    </md-toolbar>
+    <div v-if="loading">
+      <md-progress-spinner :md-diameter="100" :md-stroke="10" md-mode="indeterminate">
+      </md-progress-spinner>
+      <h3 clas="md-title">Loading</h3>
+    </div>
+    <md-card v-if="!choice">
+      <md-card-header>
+        <div class="md-title">Welcome</div>
+      </md-card-header>
+
+      <md-card-content>
+        You can select a pool on the top, then once in a pool you can click each user square to
+        bring up that users scores.
+      </md-card-content>
+      <md-card-content>
+        The refresh and winners button will become active after the start of the game.
+      </md-card-content>
+    </md-card>
+    <SquaresBoard v-if="choice" :gameDay="gameDay" :pool="choice" :picks="picks"
+                  :afc="afc" :nfc="nfc" :scores="scores"></SquaresBoard>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import axios from 'axios';
+import moment from 'moment-timezone';
+import SquaresBoard from './components/SquaresBoard.vue';
+
+const API_PATH = 'https://api.db94.io/squar';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    SquaresBoard,
+  },
+  data() {
+    return {
+      choice: null,
+      loading: false,
+      scores: [],
+      picks: {},
+      gameDay: moment.now() < moment.tz('2019-02-03T13:00:00', 'America/Los_Angeles'),
+      afc: {},
+      nfc: {},
+    };
+  },
+  methods: {
+    choose(choice) {
+      this.choice = null;
+      this.loading = true;
+      axios.get(`${API_PATH}/picks/${choice}`).then((response) => {
+        this.picks = response.data.picks;
+        this.afc = response.data.afc;
+        this.nfc = response.data.nfc;
+        this.choice = choice;
+        this.loading = false;
+        if (!this.gameDay) {
+          axios.get(`${API_PATH}/scores`).then((scoreResponse) => {
+            this.scores = scoreResponse.data.scores;
+          });
+        }
+      });
+    },
+  },
+};
 </script>
 
 <style>
@@ -23,6 +79,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  max-height: 100vh;
+}
+
+.divider {
+  border: 1px solid var(--md-theme-default-divider-on-background, rgba(255,255,255,0.12));
+  text-align: center;
+}
+
+.thickBottom {
+  border-bottom: 1px solid var(--md-theme-default-divider-on-background, rgba(255,255,255,0.5));
+}
+.thickRight {
+  border-right: 1px solid var(--md-theme-default-divider-on-background, rgba(255,255,255,0.5));
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default-dark.css';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.use(VueMaterial);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
-  template: '<App/>'
-})
+  template: '<App/>',
+});
