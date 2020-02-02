aa6eb8394b36ffac1738a9c4ed28cbe5f2234383
Martin <martinlcalvert@gmail.com>
2020-02-02 14:30:46 -0800

    Update for 2020 superbowl

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,268 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <md-toolbar class="md-dense">
+      <md-button class="md-icon-button md-raised" @click="home()"><md-icon>home</md-icon></md-button>
+      <md-button class="md-icon-button md-primary md-raised" v-if="choice" @click="showDialog = true"><md-icon>attach_money</md-icon></md-button>
+      <md-button class="md-icon-button md-primary md-raised" @click="searchPage()"><md-icon>search</md-icon></md-button>
+      <md-button class="md-raised md-primary" @click="choose('1')">$1</md-button>
+      <md-button class="md-raised md-primary" @click="choose('10')">$10</md-button>
+      <md-button class="md-raised md-primary" @click="choose('25')">$25</md-button>
+    </md-toolbar>
+    <div v-if="loading">
+      <md-progress-spinner :md-diameter="100" :md-stroke="10" md-mode="indeterminate">
+      </md-progress-spinner>
+      <h3 clas="md-title">Loading</h3>
+    </div>
+    <md-card v-if="search">
+      <md-card-header>
+        <div class="md-title">Search</div>
+      </md-card-header>
+
+      <md-card-content>
+        <md-field>
+          <label for="users">Choose Entries</label>
+          <md-select v-model="selectedUsers" name="users" id="users" multiple>
+            <md-option v-for="entry in entries" v-bind:key="entry" v-bind:value="entry">{{entry}}</md-option>
+          </md-select>
+        </md-field>
+
+        <md-table v-for="user in selectedUsers" v-bind:key="user" md-card class="searchTable">
+          <md-table-row>
+            <md-table-head>Name</md-table-head>
+            <md-table-head>Pool</md-table-head>
+            <md-table-head>Q1</md-table-head>
+            <md-table-head>Q2</md-table-head>
+            <md-table-head>Q3</md-table-head>
+            <md-table-head>2M</md-table-head>
+            <md-table-head>Q4</md-table-head>
+          </md-table-row>
+          <md-table-row v-for="(picks, index) in pools.entries[user]['25']" v-bind:key="user + '25' + index">
+            <md-table-cell>{{user}}</md-table-cell>
+            <md-table-cell>$25</md-table-cell>
+            <md-table-cell>SF: {{picks[0][0]}}, KC: {{picks[0][1]}}</md-table-cell>
+            <md-table-cell>SF: {{picks[1][0]}}, KC: {{picks[1][1]}}</md-table-cell>
+            <md-table-cell>SF: {{picks[2][0]}}, KC: {{picks[2][1]}}</md-table-cell>
+            <md-table-cell>SF: {{picks[3][0]}}, KC: {{picks[3][1]}}</md-table-cell>
+            <md-table-cell>SF: {{picks[4][0]}}, KC: {{picks[4][1]}}</md-table-cell>
+          </md-table-row>
+
+          <md-table-row v-for="(picks, index) in pools.entries[user]['10']" v-bind:key="user + '10' + index">
+            <md-table-cell>{{user}}</md-table-cell>
+            <md-table-cell>$10</md-table-cell>
+            <md-table-cell>SF: {{picks[0][0]}}, KC: {{picks[0][1]}}</md-table-cell>
+            <md-table-cell>SF: {{picks[1][0]}}, KC: {{picks[1][1]}}</md-table-cell>
+            <md-table-cell>SF: {{picks[2][0]}}, KC: {{picks[2][1]}}</md-table-cell>
+            <md-table-cell>SF: {{picks[3][0]}}, KC: {{picks[3][1]}}</md-table-cell>
+            <md-table-cell>SF: {{picks[4][0]}}, KC: {{picks[4][1]}}</md-table-cell>
+          </md-table-row>
+
+          <md-table-row v-for="(picks, index) in pools.entries[user]['1']" v-bind:key="user + '1' + index">
+            <md-table-cell>{{user}}</md-table-cell>
+            <md-table-cell>$1</md-table-cell>
+            <md-table-cell>SF: {{picks[0][0]}}, KC: {{picks[0][1]}}</md-table-cell>
+            <md-table-cell>SF: {{picks[1][0]}}, KC: {{picks[1][1]}}</md-table-cell>
+            <md-table-cell>SF: {{picks[2][0]}}, KC: {{picks[2][1]}}</md-table-cell>
+            <md-table-cell>SF: {{picks[3][0]}}, KC: {{picks[3][1]}}</md-table-cell>
+            <md-table-cell>SF: {{picks[4][0]}}, KC: {{picks[4][1]}}</md-table-cell>
+          </md-table-row>
+        </md-table>
+      </md-card-content>
+    </md-card>
+    <md-card v-if="!search && !choice">
+      <md-card-header>
+        <div class="md-title">Super Bowl Squares</div>
+      </md-card-header>
+
+      <md-card-content>
+        <div class="md-subheading">How To Use</div>
+        You can select a pools on the top, then once in a pool you can click each user square to
+        bring up that users scores.<br/>
+        The refresh and winners button will become active after the start of the game.<br/>
+        Or choose the search icon and view just the picks you select.
+      </md-card-content>
+
+      <md-card-header>
+        <div class="md-title">Mailing List</div>
+      </md-card-header>
+      <md-card-content>
+        If you want to get the emails about superbowl squares next year please sign up for the mailing list. The email might go to your spam so make sure to check for the confirmation email there also.
+        <form action="https://ml.kundenserver.de/cgi-bin/mailinglist.cgi" method="POST" target="_blank">
+          <input checked name="subscribe_r" type="radio" value="subscribe">
+          Yes, I want to become a member of the mailing list squares@db94.io.
+          <br />
+          <input name="subscribe_r" type="radio" value="unsubscribe">
+          Please remove me from mailing list squares@db94.io.
+          <br />
+          Enter your email address:
+          <br />
+          <input maxlength="51" name="mailaccount_r" size="51" type="text">
+          <br />
+          Please confirm your email address:
+          <br />
+          <input maxlength="51" name="mailaccount2_r" size="51" type="text">
+          <br />
+          <input type="SUBMIT" value="Send">
+          <br />
+          <input type="RESET" value="Reset">
+          <hr />
+          <input name="FBMLNAME" type="hidden" value="squares@db94.io">
+          <br />
+          <input name="FBLANG" type="hidden" value="en">
+          <br />
+          <input name="FBURLERROR_L" type="hidden" value="https://ml.kundenserver.de/mailinglist/error.en.html">
+          <br />
+          <input name="FBURLSUBSCRIBE_L" type="hidden" value="https://ml.kundenserver.de/mailinglist/subscribe.en.html">
+          <br />
+          <input name="FBURLUNSUBSCRIBE_L" type="hidden" value="https://ml.kundenserver.de/mailinglist/unsubscribe.en.html">
+          <br />
+          <input name="FBURLINVALID_L" type="hidden" value="https://ml.kundenserver.de/mailinglist/invalid.en.html">
+        </form>
+      </md-card-content>
+    </md-card>
+    <md-card v-if="false">
+      <md-card-content>
+        <md-field>
+          <label for="entries">Select Entries To Watch</label>
+          <md-select v-model="selectedEntries" name="entries" multiple>
+            <md-option v-for="entry in options" v-bind:value="entry" v-bind:key="entry">{{entry}}</md-option>
+          </md-select>
+        </md-field>
+      </md-card-content>
+    </md-card>
+    <md-card v-if="selectedEntries.length > 0">
+      <md-card-content>
+        <md-card-header>
+          <div class="md-title">$1 Pool</div>
+        </md-card-header>
+        <md-table>
+          <md-table-row>
+            <md-table-cell class="smaller">Quarter</md-table-cell>
+            <md-table-cell>Winner</md-table-cell>
+            <md-table-cell>Reverse</md-table-cell>
+            <md-table-cell>Touching Winner</md-table-cell>
+          </md-table-row>
+        </md-table>
+      </md-card-content>
+    </md-card>
+    <md-dialog :md-active.sync="showDialog">
+      <md-dialog-title>Payouts</md-dialog-title>
+
+      <md-card>
+        <md-card-content>
+          <md-table>
+            <md-table-row>
+              <md-table-cell class="smaller">Quarter</md-table-cell>
+              <md-table-cell class="smaller">Winner</md-table-cell>
+              <md-table-cell class="smaller">Reverse</md-table-cell>
+              <md-table-cell class="smaller">Touching Winner</md-table-cell>
+            </md-table-row>
+            <md-table-row>
+              <md-table-cell class="smaller">1st</md-table-cell>
+              <md-table-cell class="smaller">${{8*num}}</md-table-cell>
+              <md-table-cell class="smaller">${{2*num}}</md-table-cell>
+              <md-table-cell class="smaller">${{1*num}}</md-table-cell>
+            </md-table-row>
+            <md-table-row>
+              <md-table-cell class="smaller">2nd</md-table-cell>
+              <md-table-cell class="smaller">${{12*num}}</md-table-cell>
+              <md-table-cell class="smaller">${{4*num}}</md-table-cell>
+              <md-table-cell class="smaller">${{1*num}}</md-table-cell>
+            </md-table-row>
+            <md-table-row>
+              <md-table-cell class="smaller">3rd</md-table-cell>
+              <md-table-cell class="smaller">${{14*num}}</md-table-cell>
+              <md-table-cell class="smaller">${{2*num}}</md-table-cell>
+              <md-table-cell class="smaller">${{1*num}}</md-table-cell>
+            </md-table-row>
+            <md-table-row>
+              <md-table-cell class="smaller">2 min</md-table-cell>
+              <md-table-cell class="smaller">${{4*num}}</md-table-cell>
+              <md-table-cell class="smaller">N/A</md-table-cell>
+              <md-table-cell class="smaller">N/A</md-table-cell>
+            </md-table-row>
+            <md-table-row>
+              <md-table-cell class="smaller">Final</md-table-cell>
+              <md-table-cell class="smaller">${{30*num}}</md-table-cell>
+              <md-table-cell class="smaller">${{8*num}}</md-table-cell>
+              <md-table-cell class="smaller">${{1*num}}</md-table-cell>
+            </md-table-row>
+          </md-table>
+          <md-dialog-actions>
+            <md-button class="md-primary" @click="showDialog = false">Close</md-button>
+          </md-dialog-actions>
+        </md-card-content>
+      </md-card>
+    </md-dialog>
+    <SquaresBoard v-if="choice" :gameDay="gameDay" :pool="choice" :pools="pools[choice]" :scores="scores"></SquaresBoard>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import moment from 'moment-timezone';
+import SquaresBoard from './components/SquaresBoard.vue';
+
+const API_PATH = 'https://api.db94.io/squares';
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    SquaresBoard,
+  },
+  data() {
+    return {
+      choice: null,
+      loading: false,
+      scores: [],
+      pools: {},
+      picks: {},
+      gameDay: moment.now() < moment.tz('2020-02-03T13:00:00', 'America/Los_Angeles'),
+      afc: {},
+      nfc: {},
+      selectedEntries: [],
+      showDialog: false,
+      search: false,
+      selectedUsers: [],
+    };
+  },
+  created: function() {
+    axios.get(`${API_PATH}/picks`).then((response) => {
+      this.pools = response.data
+      this.loading = false;
+      if (!this.gameDay) {
+        axios.get(`${API_PATH}/scores`).then((scoreResponse) => {
+          this.scores = scoreResponse.data.scores;
+        });
+      }
+    });
+  },
+  /* eslint-disable no-console */
+  methods: {
+    choose(choice) {
+      this.search = false;
+      this.choice = choice;
+    },
+    home() {
+      this.choice = null;
+      this.search = false;
+    },
+    searchPage() {
+      this.search = true;
+      this.choice = null;
+    }
+  },
+  computed: {
+    entries() {
+      return Object.keys(this.pools.entries).sort();
+    },
+    num() {
+      if (this.choice) {
+        return Number(this.choice);
+      }
+      return 1;
+    }
+  },
+};
 </script>
 
 <style>
@@ -23,6 +272,31 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  max-height: 100vh;
+}
+
+.divider {
+  border: 1px solid var(--md-theme-default-divider-on-background, rgba(255,255,255,0.12));
+  text-align: center;
+}
+
+.thickBottom {
+  border-bottom: 1px solid var(--md-theme-default-divider-on-background, rgba(255,255,255,0.5));
+}
+.thickRight {
+  border-right: 1px solid var(--md-theme-default-divider-on-background, rgba(255,255,255,0.5));
+}
+
+.md-table-cell-container {
+  padding-left: 12px !important;
+  padding-right: 12px !important;
+}
+
+.md-button.md-primary {
+  min-width: 30px !important;
+}
+
+.searchTable {
+  text-align: left !important;
 }
 </style>
---
+++ src/components/SquaresBoard.vue
@@ -0,0 +1,333 @@
+<template>
+  <md-table md-card>
+    <md-table-toolbar>
+      <h1 class="md-title" style="flex: 1">{{pool}} Pool</h1>
+      <md-button class="md-raised md-accent" v-on:click="getScores()" :disabled="gameDay">
+        Refresh <md-icon>cached</md-icon>
+      </md-button>
+      <md-button class="md-raised md-accent" v-on:click="getWinners()" :disabled="gameDay">
+        Winners
+      </md-button>
+    </md-table-toolbar>
+
+    <md-table-row class="noHover">
+      <md-table-cell v-for="(number, index) in afc.q1" v-bind:key="number + index"
+                     v-bind:class="{ divider: number, noHover: true, thickRight: (index == 4)}">
+                     {{number}}
+      </md-table-cell>
+    </md-table-row>
+
+    <md-table-row class="noHover">
+      <md-table-cell v-for="(number, index) in afc.q2" v-bind:key="number + index"
+                     v-bind:class="{ divider: number, thickRight: (index == 4)}">
+                     {{number}}
+      </md-table-cell>
+    </md-table-row>
+
+    <md-table-row class="noHover">
+      <md-table-cell v-for="(number, index) in afc.q3" v-bind:key="number + index"
+                     v-bind:class="{ divider: number, thickRight: (index == 4)}">
+                     {{number}}
+      </md-table-cell>
+    </md-table-row>
+
+    <md-table-row class="noHover">
+      <md-table-cell v-for="(number, index) in afc.m2" v-bind:key="number + index"
+                     v-bind:class="{ divider: number, thickRight: (index == 4)}">
+                     {{number}}
+      </md-table-cell>
+    </md-table-row>
+
+    <md-table-row class="noHover thickBottom">
+      <md-table-cell v-for="(number, index) in afc.q4" v-bind:key="number + index"
+            v-bind:class="{ divider: number, thickBottom: true, thickRight: (index == 4) }">
+                     {{number}}
+      </md-table-cell>
+    </md-table-row>
+
+    <md-table-row v-for="(pick, y) in picks" v-bind:key="'row' + y">
+      <md-table-cell class="divider">{{nfc.q1[y]}}</md-table-cell>
+      <md-table-cell class="divider">{{nfc.q2[y]}}</md-table-cell>
+      <md-table-cell class="divider">{{nfc.q3[y]}}</md-table-cell>
+      <md-table-cell class="divider">{{nfc.m2[y]}}</md-table-cell>
+      <md-table-cell class="divider thickRight">{{nfc.q4[y]}}</md-table-cell>
+      <md-table-cell class="divider hover" v-for="(item, x) in pick" v-bind:key="item + y + x"
+                     @click.native="toggle(item, x, y)">{{item}}</md-table-cell>
+    </md-table-row>
+    <md-dialog :md-active.sync="showModal">
+      <md-table>
+        <md-table-row class="">
+          <md-table-head>{{pick}}</md-table-head>
+          <md-table-head class="">AFC - NFC</md-table-head>
+        </md-table-row>
+        <md-table-row>
+          <md-table-head>1st Quarter</md-table-head>
+          <md-table-cell>{{afc['q1'][x+5]}} - {{nfc['q1'][y]}}</md-table-cell>
+        </md-table-row>
+
+        <md-table-row>
+          <md-table-head>2nd Quarter</md-table-head>
+          <md-table-cell>{{afc['q2'][x+5]}} - {{nfc['q2'][y]}}</md-table-cell>
+        </md-table-row>
+
+        <md-table-row>
+          <md-table-head>3rd Quarter</md-table-head>
+          <md-table-cell>{{afc['q3'][x+5]}} - {{nfc['q3'][y]}}</md-table-cell>
+        </md-table-row>
+
+        <md-table-row>
+          <md-table-head>2 Minute Warning</md-table-head>
+          <md-table-cell>{{afc['m2'][x+5]}} - {{nfc['m2'][y]}}</md-table-cell>
+        </md-table-row>
+
+        <md-table-row>
+          <md-table-head>4th Quarter</md-table-head>
+          <md-table-cell>{{afc['q4'][x+5]}} - {{nfc['q4'][y]}}</md-table-cell>
+        </md-table-row>
+      </md-table>
+
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="showModal = false">Close</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+    <md-dialog v-if="showWinnersModal" :md-active.sync="showWinnersModal">
+      <md-table>
+        <md-table-row class="">
+          <md-table-head>Quarter</md-table-head>
+          <md-table-head>Winner</md-table-head>
+          <md-table-head>Reverse</md-table-head>
+          <md-table-head>Touching</md-table-head>
+        </md-table-row>
+        <md-table-row>
+          <md-table-head>1st</md-table-head>
+          <md-table-cell>
+            {{(picks[winners.q1.winner.y] || [])[winners.q1.winner.x]}} ({{this.pool * 8}})
+          </md-table-cell>
+          <md-table-cell>
+            {{(picks[winners.q1.reverse.y] || [])[winners.q1.reverse.x]}} ({{this.pool * 2}})
+          </md-table-cell>
+          <md-table-cell>
+            {{getTouchingSquares(winners.q1.winner.x, winners.q1.winner.y)}} ({{this.pool}})
+          </md-table-cell>
+        </md-table-row>
+
+        <md-table-row>
+          <md-table-head>2nd</md-table-head>
+          <md-table-cell>
+            {{(picks[winners.q2.winner.y] || [])[winners.q2.winner.x]}} ({{this.pool * 12}})
+          </md-table-cell>
+          <md-table-cell>
+            {{(picks[winners.q2.reverse.y] || [])[winners.q2.reverse.x]}} ({{this.pool * 4}})
+          </md-table-cell>
+          <md-table-cell>
+            {{getTouchingSquares(winners.q2.winner.x, winners.q2.winner.y)}} ({{this.pool}})
+          </md-table-cell>
+        </md-table-row>
+
+        <md-table-row>
+          <md-table-head>3rd</md-table-head>
+          <md-table-cell>
+            {{(picks[winners.q3.winner.y] || [])[winners.q3.winner.x]}} ({{this.pool * 14}})
+          </md-table-cell>
+          <md-table-cell>
+            {{(picks[winners.q3.reverse.y] || [])[winners.q3.reverse.x]}} ({{this.pool * 2}})
+          </md-table-cell>
+          <md-table-cell>
+            {{getTouchingSquares(winners.q3.winner.x, winners.q3.winner.y)}} ({{this.pool}})
+          </md-table-cell>
+        </md-table-row>
+
+        <md-table-row>
+          <md-table-head>2m</md-table-head>
+          <md-table-cell>
+            {{(picks[winners.m2.winner.y] || [])[winners.m2.winner.x]}} ({{this.pool * 4}})
+          </md-table-cell>
+          <md-table-cell>
+            <!-- {{(picks[winners.m2.reverse.y] || [])[winners.m2.reverse.x]}} -->
+          </md-table-cell>
+          <md-table-cell>
+            <!-- {{getTouchingSquares(winners.m2.winner.x, winners.m2.winner.y)}} -->
+          </md-table-cell>
+        </md-table-row>
+
+        <md-table-row>
+          <md-table-head>4th</md-table-head>
+          <md-table-cell>
+            {{(picks[winners.q4.winner.y] || [])[winners.q4.winner.x]}} ({{this.pool * 30}})
+          </md-table-cell>
+          <md-table-cell>
+            {{(picks[winners.q4.reverse.y] || [])[winners.q4.reverse.x]}} ({{this.pool * 8}})
+          </md-table-cell>
+          <md-table-cell>
+            {{getTouchingSquares(winners.q4.winner.x, winners.q4.winner.y)}} ({{this.pool}})
+          </md-table-cell>
+        </md-table-row>
+      </md-table>
+
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="showWinnersModal = false">Close</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+  </md-table>
+</template>
+
+<script>
+import axios from 'axios';
+
+const API_PATH = 'https://api.db94.io/squares';
+
+export default {
+  name: 'App',
+  props: ['pool', 'pools', 'gameDay', 'scores'],
+  data() {
+    return {
+      showModal: false,
+      showWinnersModal: false,
+      pick: null,
+      x: null,
+      y: null,
+      localScores: null,
+      winners: {
+        q1: {
+          winner: {
+            x: null,
+            y: null,
+          },
+          reverse: {
+            x: null,
+            y: null,
+          },
+        },
+        q2: {
+          winner: {
+            x: null,
+            y: null,
+          },
+          reverse: {
+            x: null,
+            y: null,
+          },
+        },
+        q3: {
+          winner: {
+            x: null,
+            y: null,
+          },
+          reverse: {
+            x: null,
+            y: null,
+          },
+        },
+        m2: {
+          winner: {
+            x: null,
+            y: null,
+          },
+          reverse: {
+            x: null,
+            y: null,
+          },
+        },
+        q4: {
+          winner: {
+            x: null,
+            y: null,
+          },
+          reverse: {
+            x: null,
+            y: null,
+          },
+        },
+      },
+    };
+  },
+  computed: {
+    localScore() {
+      return this.localScores ? this.localScores : this.scores;
+    },
+    afc() {
+      return this.pools.afc;
+    },
+    nfc() {
+      return this.pools.nfc;
+    },
+    picks() {
+      return this.pools.picks;
+    }
+  },
+  methods: {
+    toggle(item, x, y) {
+      this.pick = item;
+      this.x = x;
+      this.y = y;
+      this.showModal = true;
+    },
+    getWinners() {
+      const winners = { ...this.winners };
+      this.localScore.forEach((score) => {
+        winners[score.period].winner.x = this.getWinner(score.afc, this.afc[score.period]) - 5;
+        winners[score.period].winner.y = this.getWinner(score.nfc, this.nfc[score.period]);
+        winners[score.period].reverse.x = this.getWinner(score.nfc, this.afc[score.period]) - 5;
+        winners[score.period].reverse.y = this.getWinner(score.afc, this.nfc[score.period]);
+      });
+      this.winners = winners;
+      this.showWinnersModal = true;
+    },
+    getWinner(score, scores) {
+      return scores.findIndex(x => x === score);
+    },
+    getTouchingSquares(x, y) {
+      const touching = [];
+      if (x !== null && x !== undefined && y !== null && y !== undefined) {
+        touching.push(this.getTouchingSquare(x + 1, y));
+        touching.push(this.getTouchingSquare(x - 1, y));
+        touching.push(this.getTouchingSquare(x, y + 1));
+        touching.push(this.getTouchingSquare(x, y - 1));
+      } else {
+        return null;
+      }
+      return touching.join(', ');
+    },
+    getTouchingSquare(x, y) {
+      let touchingX = x;
+      let touchingY = y;
+      if (x === 10) {
+        touchingX = 0;
+      } else if (x === -1) {
+        touchingX = 9;
+      }
+
+      if (y === 10) {
+        touchingY = 0;
+      } else if (y === -1) {
+        touchingY = 9;
+      }
+
+      return this.picks[touchingY][touchingX];
+    },
+    getScores() {
+      axios.get(`${API_PATH}/scores`).then((scoreResponse) => {
+        this.localScores = scoreResponse.data.scores;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.divider {
+  border: 1px solid var(--md-theme-default-divider-on-background, rgba(255,255,255,0.12));
+  text-align: center;
+}
+
+.thickBottom {
+  border-bottom: 1px solid var(--md-theme-default-divider-on-background, rgba(255,255,255,0.5));
+}
+.thickRight {
+  border-right: 1px solid var(--md-theme-default-divider-on-background, rgba(255,255,255,0.5));
+}
+
+.hover:hover {
+  background-color: #ff5252 !important;
+}
+</style>
